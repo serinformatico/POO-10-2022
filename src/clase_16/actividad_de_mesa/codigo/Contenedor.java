95cd6c491cec5f6f01b98b3c01cfbc7b86ce0285
Lic. Sergio Regalado Alessi <serdigital@live.com.ar>
2022-11-22 17:43:07 -0300

    Agrego soluciones de clase 16

--- src/clase_16/actividad_de_mesa/codigo/Contenedor.java
+++ src/clase_16/actividad_de_mesa/codigo/Contenedor.java
@@ -1,22 +1,22 @@
 package clase_16.actividad_de_mesa.codigo;
 
-public class Contenedor implements Comparable {
+import java.util.Comparator;
+
+public class Contenedor implements Comparable<Contenedor>, Comparator<Contenedor> {
+
+    // Atributos privados
     private int id;
     private String pais;
-    private Boolean materialesPeligrosos;
+    private boolean esMaterialPeligroso;
 
-    public Contenedor(int id, String pais, Boolean materialesPeligrosos) {
-        this.id = id;
-        this.pais = pais;
-        this.materialesPeligrosos = materialesPeligrosos;
+    // Constructor
+    public Contenedor(int id, String pais, boolean esMaterialPeligroso) {
+        this.id                  = id;
+        this.pais                = pais;
+        this.esMaterialPeligroso = esMaterialPeligroso;
     }
 
-    @Override
-    public int compareTo(Object o) {
-        Contenedor contendor = (Contenedor) o;
-        return this.id - contendor.getId();
-    }
-
+    // Getters
     public int getId() {
         return this.id;
     }
@@ -25,7 +25,42 @@ public class Contenedor implements Comparable {
         return this.pais;
     }
 
-    public Boolean contieneMaterialesPeligrosos() {
-        return this.materialesPeligrosos;
+    public boolean getEsMaterialPeligroso() {
+        return this.esMaterialPeligroso;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setEsMaterialPeligroso(boolean esMaterialPeligroso) {
+        this.esMaterialPeligroso = esMaterialPeligroso;
+    }
+
+    // Métodos de comparación necesarios para ordenar una collection
+    @Override
+    public int compareTo(Contenedor contenedor) {
+        if (this.getId() < contenedor.getId()) {
+            return -1;
+        } else if (this.getId() > contenedor.getId()) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public int compare(Contenedor x, Contenedor y) {
+        return x.compareTo(y);
+    }
+
+    @Override
+    public String toString() {
+        return "Contenedor = id: " + id + ", país: '" + pais + ", es material peligroso: " + esMaterialPeligroso;
     }
 }
--- src/clase_16/actividad_de_mesa/codigo/Puerto.java
+++ src/clase_16/actividad_de_mesa/codigo/Puerto.java
@@ -1,31 +1,44 @@
 package clase_16.actividad_de_mesa.codigo;
 
-import java.util.TreeSet;
+import clase_16.actividad_en_vivo.codigo.Jugador;
+
+import java.util.ArrayList;
 
 public class Puerto {
-    private TreeSet<Contenedor> contenedores = new TreeSet<>();
 
-    public void permiteEntrada(Contenedor contenedor) {
-        contenedores.add(contenedor);
+    // Atributo privado
+    private ArrayList<Contenedor> contenedores = new ArrayList();
+
+    // Constructor
+    public void permitirEntradaDeContenedor(Contenedor contenedor) {
+        this.contenedores.add(contenedor);
     }
 
     public void mostrarContenedores() {
-        System.out.println("CONTENEDORES:");
-        for(Contenedor c : contenedores) {
-            System.out.println("Id: " + c.getId());
-            System.out.println("Procedencia: " + c.getPais());
-            System.out.println("Contiene materiales peligrosos: " + c.contieneMaterialesPeligrosos());
-            System.out.println("-----------");
+        for (Contenedor contenedor : this.contenedores) {
+            System.out.println(contenedor.toString());
         }
     }
 
-    public int contenedoresPeligrosos() {
-        int contenedores = 0;
-        for(Contenedor c : this.contenedores) {
-            if(c.contieneMaterialesPeligrosos() && "Desconocida".equals(c.getPais())) {
-                contenedores ++;
+    public int obtenerContenedoresPeligrosos() {
+        int cantidadDeContenedoresPeligrosos = 0;
+
+        for (Contenedor contenedor : this.contenedores) {
+            if (contenedor.getEsMaterialPeligroso() == true && contenedor.getPais().equals("Desconocida")) {
+                cantidadDeContenedoresPeligrosos++;
             }
         }
+
+        return cantidadDeContenedoresPeligrosos;
+    }
+
+    // Getter
+    public ArrayList<Contenedor> getContenedores() {
         return contenedores;
+    }
+
+    // Setter
+    public void setContenedores(ArrayList<Contenedor> contenedores) {
+        this.contenedores = contenedores;
     }
 }
